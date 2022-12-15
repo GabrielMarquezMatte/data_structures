364d9a4c4c3e434fe084fe870dfac62b3d698033
Gabriel Matte <gabrielandremarquez.matte@gmail.com>
2022-12-15 12:15:08 -0300

    Coloca os testes em threads diferentes

--- test/test.c
+++ test/test.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <Windows.h>
 #include "structures.h"
 int vector_string_test(int size)
 {
@@ -11,21 +12,19 @@ int vector_string_test(int size)
     {
         vector_push_back(v, &i, sizeof(int));
     }
-    string_t *result_str = malloc(sizeof(string_t));
-    string_init_alloc(result_str, 1024 * 1024);
     for (i = 0; i < size; i++)
     {
         int *value = vector_get(v, i, sizeof(int));
-        int size_for_buffer = snprintf(NULL, 0, "vector[%d] = %d\n", i, *value);
-        char *buffer = malloc(size_for_buffer + 1);
-        sprintf(buffer, "vector[%d] = %d\n", i, *value);
-        string_concat(result_str, buffer, strlen(buffer));
+        if(*value != i)
+        {
+            fwrite("Vector test failed\n", 1, 19, stdout);
+            fflush(stdout);
+            return 0;
+        }
     }
     vector_free(v);
     free(v);
-    string_free(result_str);
-    free(result_str);
-    fwrite("Vector test passed", 1, 18, stdout);
+    fwrite("Vector test passed\n", 1, 19, stdout);
     fflush(stdout);
     return 1;
 }
@@ -41,21 +40,25 @@ int hash_table_test(int size)
     i--;
     int j = 1000;
     hash_table_insert(ht, &i, &j, sizeof(int), sizeof(int));
-    string_t *result_str = malloc(sizeof(string_t));
-    string_init_alloc(result_str, 1024 * 1024);
     for (i = 0; i < size; i++)
     {
         int *value = hash_table_get(ht, &i, sizeof(int));
-        int size_for_buffer = snprintf(NULL, 0, "hash_table[%d] = %d\n", i, *value);
-        char *buffer = malloc(size_for_buffer + 1);
-        sprintf(buffer, "hash_table[%d] = %d\n", i, *value);
-        string_concat(result_str, buffer, strlen(buffer));
+        if(*value != i && i != size - 1)
+        {
+            fwrite("Hash table test failed\n", 1, 23, stdout);
+            fflush(stdout);
+            return 0;
+        }
+        else if(*value != 1000 && i == size - 1)
+        {
+            fwrite("Hash table test failed\n", 1, 23, stdout);
+            fflush(stdout);
+            return 0;
+        }
     }
-    string_free(result_str);
-    free(result_str);
     hash_table_free(ht);
     free(ht);
-    fwrite("Hash table test passed", 1, 22, stdout);
+    fwrite("Hash table test passed\n", 1, 23, stdout);
     fflush(stdout);
     return 1;
 }
@@ -69,21 +72,19 @@ int set_test(int size)
     }
     i--;
     set_type_add(s, &i);
-    string_t *result_str = malloc(sizeof(string_t));
-    string_init_alloc(result_str, 1024 * 1024);
     for (i = 0; i < size; i++)
     {
         int *value = set_type_get(s, i);
-        int size_for_buffer = snprintf(NULL, 0, "set[%d] = %d\n", i, *value);
-        char *buffer = malloc(size_for_buffer + 1);
-        sprintf(buffer, "set[%d] = %d\n", i, *value);
-        string_concat(result_str, buffer, strlen(buffer));
+        if(*value != i)
+        {
+            fwrite("Set test failed\n", 1, 16, stdout);
+            fflush(stdout);
+            return 0;
+        }
     }
-    string_free(result_str);
-    free(result_str);
     set_type_free(s);
     free(s);
-    fwrite("Set test passed", 1, 15, stdout);
+    fwrite("Set test passed\n", 1, 16, stdout);
     fflush(stdout);
     return 1;
 }
@@ -106,8 +107,6 @@ int hash_vector_test(int size)
         sprintf(buffer, "Vector_%d", i);
         hash_table_insert(ht, buffer, v, strlen(buffer), sizeof(vector_t));
     }
-    string_t *result_str = malloc(sizeof(string_t));
-    string_init_alloc(result_str, 1024 * 1024);
     for (int i = 0; i < size; i++)
     {
         int size_for_buffer = snprintf(NULL, 0, "Vector_%d", i);
@@ -117,56 +116,67 @@ int hash_vector_test(int size)
         for (int j = 0; j < 10; j++)
         {
             int *value = vector_get(v, j, sizeof(int));
-            int size_for_buffer = snprintf(NULL, 0, "hash_table[%d][%d] = %d\n", i, j, *value);
-            char *buffer = malloc(size_for_buffer + 1);
-            sprintf(buffer, "hash_table[%d][%d] = %d\n", i, j, *value);
-            string_concat(result_str, buffer, strlen(buffer));
+            if(*value != j)
+            {
+                fwrite("Hash vector test failed\n", 1, 24, stdout);
+                fflush(stdout);
+                return 0;
+            }
         }
     }
-    string_print(result_str);
-    string_free(result_str);
-    free(result_str);
     hash_table_free(ht);
     free(ht);
-    fwrite("Hash vector test passed", 1, 23, stdout);
+    fwrite("Hash vector test passed\n", 1, 24, stdout);
     fflush(stdout);
     return 1;
 }
 int main(int argc, char **argv)
 {
     int size = 100;
+    int runned = 0;
     if (argc > 1)
     {
+        HANDLE *thread_array = malloc(sizeof(HANDLE) * (argc - 2));
         size = atoi(argv[1]);
         for (int i = 2; i < argc; i++)
         {
             if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0)
             {
                 printf("Usage: %s size [-v --vector] [--hash] [--set] [--hash_vector]\n", argv[0]);
-                return 0;
+                return 1;
             }
             if (strcmp(argv[i], "-v") == 0 || strcmp(argv[i], "--vector") == 0)
             {
-                vector_string_test(size);
-                return 0;
+                thread_array[i - 2] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)vector_string_test,(LPVOID)size, 0, NULL);
+                runned = 1;
             }
             if (strcmp(argv[i], "--hash") == 0)
             {
-                hash_table_test(size);
-                return 0;
+                thread_array[i - 2] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)hash_table_test,(LPVOID)size, 0, NULL);
+                runned = 1;
             }
             if (strcmp(argv[i], "--set") == 0)
             {
-                set_test(size);
-                return 0;
+                thread_array[i - 2] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)set_test,(LPVOID)size, 0, NULL);
+                runned = 1;
             }
             if (strcmp(argv[i], "--hash_vector") == 0)
             {
-                hash_vector_test(size);
-                return 0;
+                thread_array[i - 2] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)hash_vector_test,(LPVOID)size, 0, NULL);
+                runned = 1;
             }
         }
+        WaitForMultipleObjects(argc - 2, thread_array, TRUE, INFINITE);
+        for(int i = 0; i < argc - 2; i++)
+        {
+            CloseHandle(thread_array[i]);
+        }
+        free(thread_array);
     }
-    printf("Usage: %s [size] [-v --vector] [--hash] [--set] [--hash_vector]\n", argv[0]);
-    return 1;
+    if (!runned)
+    {
+        printf("Usage: %s [size] [-v --vector] [--hash] [--set] [--hash_vector]\n", argv[0]);
+        return 1;
+    }
+    return 0;
 }
